121c66bcc34991cd739b962a0b36be7883255b94
deex91 <deex91@gmail.com>
2015-01-28 00:18:21 +0100

    Improve profile site.

--- static/javascripts/profiles/controllers/profile.controller.js
+++ static/javascripts/profiles/controllers/profile.controller.js
@@ -9,15 +9,16 @@
     .module('thinkster.profiles.controllers')
     .controller('ProfileController', ProfileController);
 
-  ProfileController.$inject = ['$location', '$routeParams', 'Profile'/*, 'Snackbar'*/];
+  ProfileController.$inject = ['$location', '$routeParams', 'Surveys', 'Profile', 'Snackbar'];
 
   /**
   * @namespace ProfileController
   */
-  function ProfileController($location, $routeParams, Profile/*, Snackbar*/) {
+  function ProfileController($location, $routeParams, Surveys, Profile, Snackbar) {
     var vm = this;
 
     vm.profile = undefined;
+    vm.surveys = [];
 
     activate();
 
@@ -30,6 +31,7 @@
       var username = $routeParams.username.substr(1);
 
       Profile.get(username).then(profileSuccessFn, profileErrorFn);
+      Surveys.get(username).then(surveysSuccessFn, surveysErrorFn);
 
       /**
       * @name profileSuccessProfile
@@ -46,7 +48,24 @@
       */
       function profileErrorFn(data, status, headers, config) {
         $location.url('/');
-        //Snackbar.error('Podany użytkownik nie istnieje.');
+        Snackbar.error('Podany użytkownik nie istnieje.');
+      }
+
+      /**
+      * @name surveysSuccessFn
+      * @desc Update `surveys` on viewmodel
+      */
+      function surveysSuccessFn(data, status, headers, config) {
+        vm.surveys = data.data;
+      }
+
+
+      /**
+        * @name surveysErrorFn
+        * @desc Show error snackbar
+        */
+      function surveysErrorFn(data, status, headers, config) {
+        Snackbar.error(data.data.error);
       }
     }
   }
